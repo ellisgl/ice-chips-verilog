511003d1936049cf3b799a0df4b3d867d7b92856
EllisGL <xellisx@gmail.com>
2025-12-09 09:14:43 -0600

    Apply upstream PR#5

--- source-7400/74244.v
+++ source-7400/74244.v
@@ -1,15 +1,23 @@
-// Octal octal buffer, non-inverting three-state outputs
+// Octal buffer with 3-state output
 
 module ttl_74244 #(parameter WIDTH = 8, DELAY_RISE = 0, DELAY_FALL = 0)
 (
-  input G1_Bar, G2_Bar,
-  input [WIDTH/2-1:0] A1,
-  input [WIDTH/2-1:0] A2,
-  output [WIDTH/2-1:0] Y1,
-  output [WIDTH/2-1:0] Y2
+  input [(WIDTH-1)/4:0] G_bar,
+  input [WIDTH-1:0] A,
+  output [WIDTH-1:0] Y
 );
 
-assign #(DELAY_RISE, DELAY_FALL) Y1 = !G1_Bar ? A1 : {(WIDTH/2-1){1'bZ}};
-assign #(DELAY_RISE, DELAY_FALL) Y2 = !G2_Bar ? A2 : {(WIDTH/2-1){1'bZ}};
+//------------------------------------------------//
+integer i;
+reg [WIDTH-1:0] computed;
+
+always @(*)
+begin
+  for (i = 0; i < WIDTH; i++)
+    computed[i] = G_bar[i/4] ? 1'bZ : A[i];
+end
+//------------------------------------------------//
+
+assign #(DELAY_RISE, DELAY_FALL) Y = computed;
 
 endmodule
